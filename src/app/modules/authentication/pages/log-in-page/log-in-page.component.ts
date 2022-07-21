036e1860b5278f77a3b9f1cc510681feff512531
ShariZhari <al227832@edu.uaa.mx>
2022-07-21 12:17:40 -0500

    Log in and Sign up pages

--- src/app/modules/authentication/pages/log-in-page/log-in-page.component.ts
+++ src/app/modules/authentication/pages/log-in-page/log-in-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthenticationService } from '@authentication/services';
 import { LoginUserDto } from '@core/dto/login-user.dto';
 import { LocalStorageService } from '@core/services/local-storage.service';
@@ -12,7 +13,8 @@ export class LogInPageComponent implements OnInit {
   user = new LoginUserDto();
   error = ''
   constructor(private authenticationService: AuthenticationService,
-              private localStorageService: LocalStorageService) {}
+              private localStorageService: LocalStorageService,
+              private router: Router) {}
 
   ngOnInit(): void {}
 
@@ -22,7 +24,8 @@ export class LogInPageComponent implements OnInit {
         this.localStorageService.token = v.data.authToken;
         this.localStorageService.user = v.data.user;
       },
-      error: (e: any) => {this.error = e.error.messages }
+      error: (e: any) => {this.error = e.error.messages },
+      complete:() => {this.router.navigate(['/']);}
   })
   }
 }
